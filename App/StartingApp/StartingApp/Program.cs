8d1ffb72ae9d21379b436269f769a46accac368a
Vladimir Iliev <v.iliev@beluga.software>
2018-11-30 16:05:35 +0200

    Init

---
+++ App/StartingApp/StartingApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartingApp
+{
+  public class Program
+  {
+    public static int CurrentID = 1;
+    public static void Main()
+    {
+      var query = new StringBuilder();
+      InsertRow(CurrentID, 0, "1", query);
+      AddChildren(CurrentID, 1, "1", query);
+    }
+
+    public static void InsertRow(int currentID, int parentID, string route, StringBuilder sb)
+    {
+
+    }
+
+    public static void AddChildren(int currentID, int parentID, string route, StringBuilder sb)
+    {
+      Console.WriteLine($"CurrentID {currentID} Parent {parentID} route {route}");
+      if (currentID < 10)
+      {
+        for (int i = 0; i < 3; i++)
+        {
+          CurrentID++;
+          var currentRoute = route + '/' + currentID;
+          AddChildren(CurrentID, parentID, currentRoute, sb);
+        }
+      }
+      else
+      {
+        var a = 1;
+      }
+    }
+  }
+}
